8b64926c9d6565576ee4d979aabbf6c9fa3e7e8d
MiltoniaSpectabilis <phalaenopsisviridis@gmail.com>
2025-01-06 03:37:45 +0100

    fix: Make username optional in UserUpdate schema

--- app/schemas/user.py
+++ app/schemas/user.py
@@ -3,21 +3,25 @@ from typing import Optional
 
 
 class UserBase(BaseModel):
-    username: constr(min_length=3, max_length=50)
+    username: str
 
 
 class UserCreate(UserBase):
     password: str = Field(..., min_length=6)
     is_admin: bool = False
+    username: constr(min_length=3, max_length=50)
 
 
 class UserUpdate(UserBase):
     is_admin: Optional[bool] = None
+    username: Optional[constr(min_length=3, max_length=50)
+                       ] = None
 
 
 class UserInDB(UserBase):
     id: int
     is_admin: bool
+    username: constr(min_length=3, max_length=50)
 
     class Config:
         from_attributes = True
